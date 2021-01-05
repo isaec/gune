d78e9128f5013a639e53fe92248890066567114f
isaac <isaac@rubenfamily.com>
2021-01-04 16:25:22 -0800

    automatic webpacking
    i should prolly gitignore the dist file, but for now its fine

--- src/game.js
+++ src/game.js
@@ -5,6 +5,7 @@ const rot = require("rot-js")
 //     alert("the rot.js library isn't supported by your browser. close the page, or many errors shall befall you.")
 // }
 
+console.log("i live")
 
 // Create a display
 let display = new rot.Display({width:60, height:30})
@@ -15,7 +16,7 @@ document.body.appendChild(container)
 //little demo to make sure all works
 let foreground, background, colors
 for (let x = 0; x <= 180; x++){
-    for (let y = 0, ascii = x; y <= 80; y++, ascii++){
+    for (let y = 0, ascii = x; y <= 80; y++, ascii+=2){
         foreground = rot.Color.toRGB([
             Math.max(0,Math.min(16,x))*Math.max(0,Math.min(128,y)),
             Math.max(0,Math.min(64,x))*Math.max(0,Math.min(64,y)),
