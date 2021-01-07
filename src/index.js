817d099cf51bd334d23bf0e76da3e6cd168dfbae
isaac <isaac@rubenfamily.com>
2021-01-06 18:24:14 -0800

    basic movement
    copy pasted code from redblob - next up is splitting it apart and letting the server update things

--- src/index.js
+++ src/index.js
@@ -5,21 +5,72 @@ import * as rot from "rot-js"
 const display = new rot.Display({
     width: 60, 
     height: 35,
-    fontSize:25,
+    fontSize:20,
     fontFamily:"metrickal, monospace",
     forceSquareRatio:true,
     })
 
-const gameFigure = document.createElement("FIGURE")
-document.body.appendChild(gameFigure)
-gameFigure.id = "gameFigure"
-console.log(gameFigure.fontFamily)
+const gameFigure = document.getElementById("gameFigure")
 gameFigure.appendChild(display.getContainer())
 
-let offset = 0
-function draw() {
-    for(let x=0;x<60;x++)for(let y=0;y<35;y++)display.draw(x,y,String.fromCharCode(offset+ y + x))
-    offset++
+const canvas = display.getContainer()
+canvas.addEventListener('keydown', handleKeyDown)
+canvas.setAttribute('tabindex', "1")
+canvas.focus()
+
+let entities = new Map();
+function createEntity(type, x, y) {
+    let id = ++createEntity.id;
+    let entity = { id, type, x, y };
+    entities.set(id, entity);
+    return entity;
+}
+createEntity.id = 0;
+
+let player = createEntity('player', 5, 4);
+
+
+function handleKeys(keyCode) {
+    const actions = {
+        [rot.KEYS.VK_RIGHT]: () => ['move', +1, 0],
+        [rot.KEYS.VK_LEFT]:  () => ['move', -1, 0],
+        [rot.KEYS.VK_DOWN]:  () => ['move', 0, +1],
+        [rot.KEYS.VK_UP]:    () => ['move', 0, -1],
+    };
+    let action = actions[keyCode];
+    return action ? action() : undefined;
 }
 
-setInterval(draw,100)
+function handleKeyDown(event) {
+    console.log(event)
+    let action = handleKeys(event.keyCode);
+    if (action) {
+        if (action[0] === 'move') {
+            let [_, dx, dy] = action;
+            player.x += dx;
+            player.y += dy;
+            draw();
+        } else {
+            throw `unhandled action ${action}`;
+        }
+        event.preventDefault();
+    }
+}
+
+function drawEntity(entity) {
+    const visuals = {
+        player: ['@', "hsl(60, 100%, 50%)"],
+        troll: ['T', "hsl(120, 60%, 50%)"],
+        orc: ['o', "hsl(100, 30%, 50%)"],
+    };
+
+    const [ch, fg, bg] = visuals[entity.type];
+    display.draw(entity.x, entity.y, ch, fg, bg);
+}
+
+function draw() {
+    display.clear();
+    for (let entity of entities.values()) {
+        drawEntity(entity);
+    }
+}
