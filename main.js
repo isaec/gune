0bbe80f015a6887603dfcdf2de8f0ba575f5795d
isaac <isaac@rubenfamily.com>
2021-01-09 12:35:15 -0800

    basic socketing properly
    tossed some game code - my git management has been a mess so far, ill work to improve it

--- main.js
+++ main.js
@@ -1,40 +1,68 @@
-const express = require('express')
-const app = express()
+//const express = require('express')
+//const app = express()
 
+const uWS = require('uWebSockets.js')
 const fs = require("fs")
+const uuid = require("uuidv4")
+const port = 4141
 
-const server = require('http').createServer(app)
+//const server = require('http').createServer(app)
+const { StringDecoder } = require('string_decoder')
+const decoder = new StringDecoder('utf8')
 
-app.get('/',function(req, res) {
- res.sendFile(__dirname + '/client/index.html')
-})
+// app.get('/',function(req, res) {
+//  res.sendFile(__dirname + '/client/index.html')
+// })
 
 
-app.use('/client',express.static(__dirname + '/client'))
-app.use('/src',express.static(__dirname + '/src'))
-app.use('/dist',express.static(__dirname + '/dist'))
+// app.use('/client',express.static(__dirname + '/client'))
+// app.use('/src',express.static(__dirname + '/src'))
+// app.use('/dist',express.static(__dirname + '/dist'))
+
+const app = uWS.App()
+
+
+
+const addFile = (app, urlPath, filePath) => {
+    if(filePath==="=") filePath=urlPath
+    app.get(urlPath, (res,req) => {
+        let file = fs.readFileSync(__dirname+filePath, function (err, data) {
+            if (err) {
+                res.end(`Error getting the file: ${err}.`)
+            } else {
+                res.end(data)
+            }
+        });
+        res.end(file)
+    })
+}
+
+addFile(app, "/", "/client/index.html")
+addFile(app, "/dist/index.js", "=")
+addFile(app, "/src/Metrickal-Regular.otf", "=")
 
 // app.get("*", function(req, res) {
 //     console.log("file requested not found")
 //     res.redirect("/client/error.html")
 // })
  
+
+
+app.ws("/ws", {
+    compression: 0,
+    maxPayloadLength: 16 * 1024 * 1024,
+    idleTimeout: 300,
+
+    open: (ws, req) => console.log("open"),
+    message: ws => console.log("message"),
+    close: (ws, code, message) => console.log("closed a ws"),
+
+
+}).listen(port, token => {
+    token ?
+    console.log(`Listening to port ${port}`) :
+    console.log(`Failed to listen to port ${port}`)
+})
+
 console.log("Server started.")
  
-let SOCKET_LIST = {}
-
- 
-const io = require('socket.io')(server)
-io.sockets.on('connection', function(socket){
-              
-        console.log('new user!')
-        let socketId = Math.random()
-        SOCKET_LIST[socketId] = socket
-              
- 
-        socket.on('disconnect',function(){      
-            delete SOCKET_LIST[socket.id]
-        })
-})
- 
-server.listen(4141)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as rot from "rot-js"
-
-
+const url = "ws://localhost:4141/ws"
+const connection = new WebSocket(url)
 
 const display = new rot.Display({
     width: 60, 
@@ -18,59 +18,15 @@ canvas.addEventListener('keydown', handleKeyDown)
 canvas.setAttribute('tabindex', "1")
 canvas.focus()
 
-let entities = new Map();
-function createEntity(type, x, y) {
-    let id = ++createEntity.id;
-    let entity = { id, type, x, y };
-    entities.set(id, entity);
-    return entity;
-}
-createEntity.id = 0;
 
-let player = createEntity('player', 5, 4);
-
-
-function handleKeys(keyCode) {
-    const actions = {
-        [rot.KEYS.VK_RIGHT]: () => ['move', +1, 0],
-        [rot.KEYS.VK_LEFT]:  () => ['move', -1, 0],
-        [rot.KEYS.VK_DOWN]:  () => ['move', 0, +1],
-        [rot.KEYS.VK_UP]:    () => ['move', 0, -1],
-    };
-    let action = actions[keyCode];
-    return action ? action() : undefined;
-}
-
-function handleKeyDown(event) {
+function handleKeyDown(event){
     console.log(event)
-    let action = handleKeys(event.keyCode);
-    if (action) {
-        if (action[0] === 'move') {
-            let [_, dx, dy] = action;
-            player.x += dx;
-            player.y += dy;
-            draw();
-        } else {
-            throw `unhandled action ${action}`;
-        }
-        event.preventDefault();
-    }
 }
 
-function drawEntity(entity) {
-    const visuals = {
-        player: ['@', "hsl(60, 100%, 50%)"],
-        troll: ['T', "hsl(120, 60%, 50%)"],
-        orc: ['o', "hsl(100, 30%, 50%)"],
-    };
 
-    const [ch, fg, bg] = visuals[entity.type];
-    display.draw(entity.x, entity.y, ch, fg, bg);
-}
+connection.onopen = () => console.log("socket connected")
+connection.onmessage = message => console.log(message.data)
 
-function draw() {
-    display.clear();
-    for (let entity of entities.values()) {
-        drawEntity(entity);
-    }
-}
+connection.onerror = error => {
+    console.log(`WebSocket error: ${error}`)
+  }
