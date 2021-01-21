67010a16b9e0cff58ded907e2c658c079a713325
isaac <isaac@rubenfamily.com>
2021-01-20 16:32:59 -0800

    unique per player world updates

--- server/world.js
+++ server/world.js
@@ -14,7 +14,7 @@ const MESSAGE_ENUM = Object.freeze({
 
 module.exports.World = function () {
     this.map = new gamemap.Map(60, 35)
-    this.entities = new Array()
+    this.entities = [] //new array
     this.getEntity = function (uuid) {
         for (const [index, entity] of this.entities.entries()) {
             if (entity.id === uuid) {
@@ -23,25 +23,49 @@ module.exports.World = function () {
         }
         return null
     }
-    this.add = function(entity){this.entities.push(entity)}
-    this.updateClients = function (app, SOCKETS) {
+    this.add = function (entity) { this.entities.push(entity) }
+    this.updateClients = function (app) {
+        //loop through the entities
+        //if they have a websocket connection, let them know what changed
+        //this is a first draft of the loop - this loop will be run often
+        //so this would be a good place to optimize network performance
+        //there is a lot wrong with this code
 
-        let worldUpdate = new WorldUpdate()
-        worldUpdate.map = this.map
-        worldUpdate.entities = this.entities
+        let playerId = [] //make array of player uuid
+        let playerWs = [] //make array of player ws that lines up
+        for (const ws of app.SOCKETS) {
+            playerId.push(ws.id)
+            playerWs.push(ws)
+        }
 
-        app.publish(MESSAGE_ENUM.SERVER_ACTION,
-            JSON.stringify({
-                type: MESSAGE_ENUM.SERVER_ACTION,
-                body: {
-                    world: worldUpdate
-                    }
-                }
-            )
-        )
+
+        for (const entity of this.entities) {
+
+            const index = playerId.indexOf(entity.id)
+
+            //if the id is present
+            if (index > -1) {
+                //make a worldUpdate
+                let worldUpdate = new WorldUpdate()
+                worldUpdate.map = this.map
+                worldUpdate.entities = this.entities
+                //send the websocket a world update
+                playerWs[index].send(
+                    JSON.stringify(
+                        {
+                            type: MESSAGE_ENUM.SERVER_ACTION,
+                            body: {
+                                world: worldUpdate
+                            }
+                        }
+                    )
+                )
+            }
+
+        }
     }
 }
 
 class WorldUpdate {
-    
+
 }
