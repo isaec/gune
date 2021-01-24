d3fbdb80e2b52e2783deae3953c16e5de906a6ca
isaac <isaac@rubenfamily.com>
2021-01-23 19:44:41 -0800

    scrollable message area

--- src/guiconsole.js
+++ src/guiconsole.js
@@ -1,17 +1,20 @@
 const HtmlColor = require("/src/color").Html
 
 module.exports.GuiConsole = function () {
-    this.maxLines = 11
+    this.maxLines = 100
     this.messages = document.querySelector("#console")
 
-    this.messages.textContent = "\n".repeat(this.maxLines)
+    //this.messages.textContent = "\n".repeat(this.maxLines)
 
     this.lines = []
 
     this.print = (ConsoleLine) => {
 
 
-        if (this.lines.length > 0 && this.lines[this.lines.length - 1].message === ConsoleLine.message) {
+        if (this.lines.length > 0 &&
+            this.lines[this.lines.length - 1].message === ConsoleLine.message &&
+            this.lines[this.lines.length - 1].stack) {
+
             this.lines[this.lines.length - 1].amount++
         }
         else {
@@ -25,28 +28,32 @@ module.exports.GuiConsole = function () {
             this.lines.length > 2 && this.lines[this.lines.length - 2].temp
         ) this.lines.splice(this.lines.length - 2, 1)
 
-        
+
 
         let displayLines = [...this.lines]
         while (displayLines.length > this.maxLines) displayLines.shift()
+        displayLines.reverse()
         this.messages.innerHTML = displayLines.join("\n")
+
+        //this.messages.scrollTop = this.messages.scrollHeight
     }
 }
 
 
-module.exports.ConsoleLine = function (message, color = [5, 5, 5], temp = false) {
+module.exports.ConsoleLine = function (message, color = [5, 5, 5], temp = false, stack = true) {
     this.message = message
     this.temp = temp
+    this.stack = stack
     this.amount = 1
     this.color = color
     this.bg = false
 
 
     this.toString = () => {
-        return this.amount > 1 ?
+        return "<div>" + (this.amount > 1 ?
             HtmlColor(`(${this.amount}x) `, [3, 3, 3])
             + HtmlColor(this.message, this.color, this.bg)
             :
-            HtmlColor(this.message, this.color, this.bg)
+            HtmlColor(this.message, this.color, this.bg)) + "</div>"
     }
 }
