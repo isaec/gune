2e667008cebbf99d988263baff02b5343e17d975
isaac <isaac@rubenfamily.com>
2021-01-24 22:49:32 -0800

    basic key repeat handling
    feels pretty good - still needs to cap inputs per second, and handle multidirectional input better

--- src/key.js
+++ src/key.js
@@ -2,29 +2,40 @@ const rot = require("rot-js")
 const guiconsole = require("/src/guiconsole")
 const MESSAGE_ENUM = require("/server/message").MESSAGE_ENUM
 
-module.exports.KeyHandler = function(world, guiConsole, screen, uuid, connection) {
+module.exports.KeyHandler = function (world, guiConsole, screen, uuid, connection) {
     this.world = world //stop doing this, write an engine
     this.guiConsole = guiConsole //also this
     this.screen = screen //engine asap
     this.uuid = uuid //this should all be in an engine class
     this.connection = connection
-    this.keymap = new Map()
+    this.keyset = new Set()
+    this.keypulse = () => {
+        for (const key of this.keyset) {
+            const action = this.keyToAction(key)
+            this.emitAction(action)
+        }
+    }
+    this.interval = setInterval(this.keypulse,150)
     this.keydown = (event) => {
-        if(event.repeat || !event.isTrusted) return
-        this.keymap[event.keyCode] = true
+        if (event.repeat || !event.isTrusted) return
+        this.keyset.add(event.keyCode)
 
-        const action = this.keyToAction(event.keyCode)
-        this.emitAction(action)
+        clearInterval(this.interval)
+        this.keypulse()
+        this.interval = setInterval(this.keypulse,150)
 
         //post handle
         event.preventDefault()
     }
     this.keyup = (event) => {
-        this.keymap[event.keyCode] = false
+        this.keyset.delete(event.keyCode)
+        // if (!this.keyset.size === 0) return
+        // if (this.interval) clearInterval(this.interval)
+        // this.interval = undefined
     }
 
     this.keyToAction = (keyCode) => {
-        switch (keyCode){ //is switch the right structure?
+        switch (keyCode) { //is switch the right structure?
             case rot.KEYS.VK_RIGHT:
             case rot.KEYS.VK_D:
                 return { type: 'move', data: [+1, 0] }
@@ -56,7 +67,7 @@ module.exports.KeyHandler = function(world, guiConsole, screen, uuid, connection
                 })()
                 const dx = action.data[0], dy = action.data[1]
                 const newX = player.x + dx, newY = player.y + dy
-                if (this.world.map.tiles.get(newX,newY) === 1 && !this.world.entityAt(newX, newY)) {
+                if (this.world.map.tiles.get(newX, newY) === 1 && !this.world.entityAt(newX, newY)) {
                     this.connection.send(JSON.stringify({
                         type: MESSAGE_ENUM.CLIENT_ACTION,
                         body: action,
