caee8f3879c4ecfc4f02d331321b4d3256cae351
isaac <isaac@rubenfamily.com>
2021-01-29 14:35:10 -0800

    grab key presses from window
    get screen relative cords for clicks, and bind the key down stuff to the whole window, so that it cant loose focus, or be affected by clicks

--- src/mouse.js
+++ src/mouse.js
@@ -1,6 +1,12 @@
-module.exports.MouseHandler = function() {
-    this.click = () => {
-        console.log("click!")
+const rot = require("rot-js")
+
+module.exports.MouseHandler = function (display) {
+    this.display = display
+    this.click = (event) => {
+
+        let [x, y] = display.eventToPosition(event)
+
+        console.log(`click at ${x}, ${y} in onscreen cords!`)
     }
     this.mousemove = () => {
         console.log("mousemove!")
