5b3bf720a30e67c65871ea0ee28844c9eebac0a9
isaac <isaac@rubenfamily.com>
2021-02-08 22:32:32 -0800

    let action functions be reapplied after JSON

--- shared/action.js
+++ shared/action.js
@@ -18,30 +18,41 @@ class Move extends Action {
         this.dx = dx
         this.dy = dy
         this.data = [dx, dy]
-    }
-
-    validate(engine, clientSide = true) {
-        const newX = this.taker.x + this.dx, newY = this.taker.y + this.dy
-        const entityAt = engine.world.getEntityAt(newX, newY)
-        if (engine.world.map.tiles.get(newX, newY) === 1 && !entityAt) {
-            if (clientSide) {
-                this.taker.x += this.dx
-                this.taker.y += this.dy
-                engine.screen.render(engine.world)
-            }
-            return true
-        } else {
-            if (clientSide) {
-                engine.guiConsole.print(
-                    entityAt ?
-                        new guiconsole.ConsoleLine(`that path is blocked by ${entityAt.type}`, [4, 3, 2], true)
-                        :
-                        new guiconsole.ConsoleLine("that path is blocked by terrain", [4, 4, 2], true)
-                )
-            }
-            return false
-        }
+        addMethods(this)
     }
 }
+
+
+function addMethods(action) {
+    switch (action.type) {
+        case "move":
+            action.validate = function (engine, clientSide = true) {
+                const newX = this.taker.x + this.dx, newY = this.taker.y + this.dy
+                const entityAt = engine.world.getEntityAt(newX, newY)
+                if (engine.world.map.tiles.get(newX, newY) === 1 && !entityAt && (Math.abs(this.taker.x - newX) <= 1 && Math.abs(this.taker.y - newY) <= 1)) {
+                    if (clientSide) {
+                        this.taker.x += this.dx
+                        this.taker.y += this.dy
+                        engine.screen.render(engine.world)
+                    }
+                    return true
+                } else {
+                    if (clientSide) {
+                        engine.guiConsole.print(
+                            entityAt ?
+                                new guiconsole.ConsoleLine(`that path is blocked by ${entityAt.type}`, [4, 3, 2], true)
+                                :
+                                new guiconsole.ConsoleLine("that path is blocked by terrain", [4, 4, 2], true)
+                        )
+                    }
+                    return false
+                }
+            }
+            break
+        default:
+            throw `unknown move type ${action.type}`
+    }
+}
+
 
 module.exports.Move = Move
