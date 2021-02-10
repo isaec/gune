bd1aea6ac31842d6bd40bcb15fcf7403417788e1
isaac <isaac@rubenfamily.com>
2021-02-10 13:36:17 -0800

    remove sentences without verbs or nouns

--- resource/process.py
+++ resource/process.py
@@ -1,5 +1,7 @@
+from os import remove
 import spacy
 from progress.bar import IncrementalBar as Bar
+import random
 
 #load toknnization stuffs
 print("loading token stuff")
@@ -15,12 +17,24 @@ bar = Bar('processing lines', max=len(filedata))
 #make and write changes to new file
 with open("dante.txt", "w") as file:
     for line in filedata:
+        #don't write non sentance lines
         if "Inferno:" in line or line == "\n":
             bar.next()
             continue
+        #remove indentation and quotations
         line = line.replace("  ", "").replace('"',"")
-        #doc = nlp(line)
+        doc = nlp(line)
+
+        pos = [tok.pos_ for tok in doc]
+
+        #don't write a sentance that lacks a noun or verb
+        if "VERB" not in pos or "NOUN" not in pos:
+            bar.next()
+            continue
         
         file.write(line)
+        #print a few lines as we go
+        #if(random.randint(0,600) == 600): print("\n"+line)
         bar.next()
 print("\ndone!")
+
