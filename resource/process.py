119e0098c0da2db979b302ff8cfe340fa3394bd3
isaac <isaac@rubenfamily.com>
2021-02-10 15:49:38 -0800

    use json for dante quotes

--- resource/process.py
+++ resource/process.py
@@ -6,23 +6,26 @@ import json
 with open("danteInferno.txt", "r") as file:
     filedata = file.readlines()
 
+# make the changes
+lineBuffer = ""
+lineList = []
+for line in filedata:
+    # don't write non sentance lines
+    if "Inferno:" in line or line == "\n":
+        continue
+    # figure out if the sentance is part of a larger block
+    if line[:2] == "  ":
+        lineBuffer += line[2:]
+    else:
+        # buffer is done
+        # make sure not to have space at start
+        if lineBuffer[:1] == " ":
+            lineBuffer = lineBuffer[1:]
+
+        lineList.append(lineBuffer[:-1])
+        lineBuffer = line
+lineList.remove("")
 
 # make and write changes to new file
-with open("dante.txt", "w") as file:
-    lineBuffer = ""
-    for line in filedata:
-        # don't write non sentance lines
-        if "Inferno:" in line or line == "\n":
-            continue
-        # figure out if the sentance is part of a larger block
-        if line[:2] == "  ":
-            lineBuffer += line[2:]
-        else:
-            #buffer is done
-            # make sure not to have space at start
-            if lineBuffer[:1] == " ":
-                lineBuffer = lineBuffer[1:]
-
-
-            file.write(lineBuffer+"\n")
-            lineBuffer = line
+with open("dante.json", "w") as file:
+    json.dump(lineList, file, indent=0)
