8f4f36dea95bedca47b658988cefabb7c6f82461
isaac <isaac@rubenfamily.com>
2021-02-10 12:15:53 -0800

    tweaks to script, remove quotation marks

--- resource/process.py
+++ resource/process.py
@@ -5,9 +5,8 @@ with open("danteInferno.txt", "r") as file:
 
 
 #make and write changes to new file
-with open("processedInferno.txt", "w") as file:
+with open("dante.txt", "w") as file:
     for line in filedata:
-        if "Inferno:" in line: continue
-        if line == "\n": continue
-        line = line.replace("  ", "")
+        if "Inferno:" in line or line == "\n": continue
+        line = line.replace("  ", "").replace('"',"")
         file.write(line)
