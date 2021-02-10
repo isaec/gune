6c98bb6dde04e3ea625980738a954775f9ab1be1
isaac <isaac@rubenfamily.com>
2021-02-10 14:51:57 -0800

    fix spacing

--- resource/process.py
+++ resource/process.py
@@ -2,19 +2,27 @@ from progress.bar import IncrementalBar as Bar
 import json
 
 
-#read file to memory
+# read file to memory
 with open("danteInferno.txt", "r") as file:
     filedata = file.readlines()
 
 
-#make and write changes to new file
+# make and write changes to new file
 with open("dante.txt", "w") as file:
     lineBuffer = ""
     for line in filedata:
-        #don't write non sentance lines
-        if "Inferno:" in line or line == "\n": continue
+        # don't write non sentance lines
+        if "Inferno:" in line or line == "\n":
+            continue
+        # figure out if the sentance is part of a larger block
         if line[:2] == "  ":
             lineBuffer += line[2:]
         else:
+            #buffer is done
+            # make sure not to have space at start
+            if lineBuffer[:1] == " ":
+                lineBuffer = lineBuffer[1:]
+
+
             file.write(lineBuffer+"\n")
             lineBuffer = line
