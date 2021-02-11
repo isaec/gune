88f304a623a263c584ebcdac538056e7ed597a8f
isaac <isaac@rubenfamily.com>
2021-02-11 15:46:46 -0800

    remove line

--- resource/sample.py
+++ resource/sample.py
@@ -1,5 +1,4 @@
 import json, random
-amount = int(input("how many samples? "))
-print()
+amount = int(input("how many samples?\n") or 2)
 with open("dante.json") as file: dante = json.load(file)
 for i in range(amount): print(f"\tnumber {i+1}:\n\n{random.choice(dante)}\n")
