ad1dbea5b523e0749e5f76f378ffa304b19decc9
isaac <isaac@rubenfamily.com>
2021-02-14 21:09:00 -0800

    use static method to apply methods to actions

--- shared/action.js
+++ shared/action.js
@@ -26,6 +26,9 @@ class Action {
     apply(taker, engine) {
         console.log(`action of type ${this.type} has no apply function`)
     }
+    static addMethods(action) {
+        console.log(`action of type ${this.type} has no addMethods function`)
+    }
 }
 
 
@@ -33,6 +36,42 @@ class Move extends Action {
     constructor(dx, dy) {
         super(Type.move, dx, dy)
         addMethods(this)
+    }
+    static addMethods(action) {
+        action.validate = function (taker, engine, clientSide = true) {
+            const newX = taker.x + this.dx, newY = taker.y + this.dy
+            const entityAt = engine.world.getEntityAt(newX, newY)
+            if ( //if the move is to an open tile
+                engine.world.map.tiles.get(newX, newY) === 1
+                && //if there is no entity in the way
+                !entityAt
+                && //if the distance being moved is only 1 tile
+                (Math.abs(taker.x - newX) <= 1 && Math.abs(taker.y - newY) <= 1)
+            ) {
+                if (clientSide) {
+                    this.apply(taker, engine)
+                    engine.screen.render(engine.world)
+                }
+                return true
+            } else {
+                if (clientSide) {
+                    engine.guiConsole.print(
+                        entityAt ?
+                            new guiconsole.ConsoleLine(`that path is blocked by \
+                            ${entityAt.type}`, [4, 3, 2], true)
+                            :
+                            new guiconsole.ConsoleLine(`that path is blocked by \
+                            terrain`, [4, 4, 2], true)
+                    )
+                }
+                return false
+            }
+        }
+
+        action.apply = function (taker,) {
+            taker.x += this.dx
+            taker.y += this.dy
+        }
     }
 }
 
@@ -47,34 +86,10 @@ class Melee extends Action {
 function addMethods(action) {
     switch (action.type) {
         case Type.move:
-            //move block
-            action.validate = function (taker, engine, clientSide = true) {
-                const newX = taker.x + this.dx, newY = taker.y + this.dy
-                const entityAt = engine.world.getEntityAt(newX, newY)
-                if (engine.world.map.tiles.get(newX, newY) === 1 && !entityAt && (Math.abs(taker.x - newX) <= 1 && Math.abs(taker.y - newY) <= 1)) {
-                    if (clientSide) {
-                        this.apply(taker, engine)
-                        engine.screen.render(engine.world)
-                    }
-                    return true
-                } else {
-                    if (clientSide) {
-                        engine.guiConsole.print(
-                            entityAt ?
-                                new guiconsole.ConsoleLine(`that path is blocked by ${entityAt.type}`, [4, 3, 2], true)
-                                :
-                                new guiconsole.ConsoleLine("that path is blocked by terrain", [4, 4, 2], true)
-                        )
-                    }
-                    return false
-                }
-            }
-
-            action.apply = function (taker,) {
-                taker.x += this.dx
-                taker.y += this.dy
-            }
-            //end move block
+            Move.addMethods(action)
+            break
+        case Type.melee:
+            Melee.addMethods(action) //not written
             break
         default:
             throw `unknown move type ${action.type}`
