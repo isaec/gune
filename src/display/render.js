0cce2fccb0a049f8203841cac5edba7e58d08874
isaac <isaac@rubenfamily.com>
2021-02-15 21:35:31 -0800

    more kosher resize binding

--- src/display/render.js
+++ src/display/render.js
@@ -9,7 +9,7 @@ module.exports.Screen = function (engine) {
     this.height = this.engine.display._options.height
 
     this.scaleDisplay = () => this.engine.display.setOptions({ fontSize: Math.floor(window.innerHeight / 36) })
-    window.onresize = this.scaleDisplay
+    window.addEventListener("resize", this.scaleDisplay)
     this.scaleDisplay()
 
 
