570f5f508e979e121ce27efd673f90d6cf85d739
isaac <isaac@rubenfamily.com>
2021-03-06 15:41:30 -0800

    use css subclass instead of js to set button color

--- src/display/button.js
+++ src/display/button.js
@@ -1,21 +1,13 @@
 const rgbstring = require("/src/display/color").rgbstring
 
 class Button {
-    constructor(id, text, hoverColor, clickColor) {
+    constructor(id, text) {
         this.id = id
         this._text = text
-        this.hoverColor = hoverColor
-        this.clickColor = clickColor
         this.button = document.querySelector(this.id)
         if (this.button === null) throw `invalid id ${this.id}`
         this._boundUpdate = this._update.bind(this)
         this.update()
-        this.button.style.backgroundColor = rgbstring(this.hoverColor)
-        // window.requestAnimationFrame(
-        //     (() => {
-        //         if (this.hoverColor) this.button.style.backgroundColor = rgbstring(this.hoverColor)
-        //     }).bind(this)
-        // )
     }
     _update() {
         this.button.innerHTML = this._text
